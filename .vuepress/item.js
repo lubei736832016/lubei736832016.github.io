ac7b17312d3ee9d588c70c84595dc7680c150ecd
lubei <736832016@qq.com>
2024-06-05 14:30:45 +0800

    容器化目录
    容器化目录

--- .vuepress/item.js
+++ .vuepress/item.js
@@ -10,6 +10,10 @@ let arr = [
     {
         "text": "项目部署与环境配置",
         "link": "/docs/projectManger/project01"
+    },
+    {
+        "text": "Docker小鲸鱼容器化基础",
+        "link": "/docs/docker/docker01"
     }
 ]
 module.exports = arr
